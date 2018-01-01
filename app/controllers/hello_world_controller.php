5b819717b35c37c04893f0a4211164c9c7f26176
Melchan <melchan@windowslive.com>
2018-01-01 21:05:55 +0200

    test viewien lisäys

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -12,16 +12,6 @@
       View::make('helloworld.html');
     }
 
-    public static function testuser(){
-      // Testaa koodiasi täällä
-      View::make('userpictures.html');
-    }
-
-    public static function testuserpicture(){
-      // Testaa koodiasi täällä
-      View::make('userpicture.html');
-    }
-
     public static function testedit(){
       // Testaa koodiasi täällä
       View::make('testedit.html');
@@ -30,6 +20,16 @@
     public static function testlogin(){
       // Testaa koodiasi täällä
       View::make('testlogin.html');
+    }
+
+    public static function testuser(){
+      // Testaa koodiasi täällä
+      View::make('userpictures.html');
+    }
+
+    public static function testuserpicture(){
+      // Testaa koodiasi täällä
+      View::make('userpicture.html');
     }
 
   }
