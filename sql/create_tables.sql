cb27a85f60183f4dd92a547bbfe5fbb2d4922f28
Melchan <melchan@windowslive.com>
2018-01-02 16:22:27 +0200

    sql bug fix

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,18 +1,18 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
 CREATE TABLE owner(
-  id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+  id int PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
   name varchar(50) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
   password varchar(50) NOT NULL,
 );
 
 CREATE TABLE category(
-  id SERIAL PRIMARY KEY,
+  id int PRIMARY KEY,
   name varchar(50) NOT NULL,
 );
 
 CREATE TABLE picture(
-  id SERIAL PRIMARY KEY,
-  owner_id INTEGER REFERENCES owner(id), -- Viiteavain Player-tauluun
+  id int PRIMARY KEY,
+  owner_id int REFERENCES owner(id), -- Viiteavain Player-tauluun
   name varchar(50) NOT NULL,
   picture bytea NOT NULL,
   description varchar(400),
@@ -20,12 +20,12 @@ CREATE TABLE picture(
 );
 
 CREATE TABLE category(
-  id SERIAL PRIMARY KEY,
+  id int PRIMARY KEY,
   name varchar(50) NOT NULL,
 );
 
 CREATE TABLE comment(
-  id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+  id int PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
   message varchar(50) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
   owner_id INTEGER REFERENCES owner(id),
   picture_id INTEGER REFERENCES picture(id),
