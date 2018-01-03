8844e2685c119486e64874c55d5129f5f33a7282
Melchan <melchan@windowslive.com>
2018-01-03 21:43:27 +0200

    etusivu test

--- app/controllers/jokecontroller.php
+++ app/controllers/jokecontroller.php
@@ -4,6 +4,6 @@ class JokeController extends BaseController {
 	
 	public static function index() {
 		$jokes = Joke::all();
-		View::make('joke/index.html', array('tasks' => $jokes));
+		View::make('index.html', array('tasks' => $jokes));
 	}
 }
