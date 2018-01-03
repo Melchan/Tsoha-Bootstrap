cafe79f221fca869063f248647519e5cc5a26d65
Melchan <melchan@windowslive.com>
2018-01-03 21:37:25 +0200

    etusivu test

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,7 +1,7 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
 INSERT INTO owner (name, password) VALUES ('Kalle', 'Kalle123'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
 INSERT INTO owner (name, password) VALUES ('Henri', 'Henri123');
-INSERT INTO joke(owner_name, title, description) VALUES ("pekka", 'Hauska kasku', 'Kuka on juusto?');
-INSERT INTO joke(owner_name, title, description) VALUES ("jukka", 'Hauska juttu', 'Kulta on juustoa arvokkaampaa!');
-INSERT INTO joke(owner_name, title, description) VALUES ("pertti", 'Hauska vitsi', 'Knock knock!');
-INSERT INTO joke(owner_name, title, description) VALUES ("pasi", 'Hauska huhu', 'Olen kuningas');
+INSERT INTO joke(owner_name, title, description) VALUES ("Kalle", 'Hauska kasku', 'Kuka on juusto?');
+INSERT INTO joke(owner_name, title, description) VALUES ("Henri", 'Hauska juttu', 'Kulta on juustoa arvokkaampaa!');
+INSERT INTO joke(owner_name, title, description) VALUES ("Kalle", 'Hauska vitsi', 'Knock knock!');
+INSERT INTO joke(owner_name, title, description) VALUES ("Henri", 'Hauska huhu', 'Olen kuningas');
