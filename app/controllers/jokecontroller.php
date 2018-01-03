4e1c4825cc83f637822a3ed933b55d76d44b676d
Melchan <melchan@windowslive.com>
2018-01-03 22:52:22 +0200

    metodit lisätty

--- app/controllers/jokecontroller.php
+++ app/controllers/jokecontroller.php
@@ -6,4 +6,24 @@ class JokeController extends BaseController {
 		$jokes = Joke::all();
 		View::make('index.html', array('jokes' => $jokes));
 	}
+
+	public static function store(){
+    $params = $_POST;
+    $joke = new Joke(array(
+      'title' => $params['title'],
+      'owner_name' => $params['owner_name'],
+      'description' => $params['description'],
+      'publisher' => $params['publisher']
+    ));
+
+    $joke->save();
+
+    Redirect::to('/' . $joke->id, array('message' => 'Vitsi on lisätty kirjastoosi!'));
+    }
+
+    public static function joke($id) {
+    	$joke = Joke::find(:id);
+    	View::make('joke.html', array('joke' =>$joke))
+    }
+
 }
